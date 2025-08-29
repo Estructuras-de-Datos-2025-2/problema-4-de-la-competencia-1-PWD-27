8fef3118688e06a1898e0a84a47fcffe857b9efc
isc <pwd27@duck.com>
2025-08-29 12:02:03 -0400

    solucion

--- main.c
+++ main.c
@@ -6,6 +6,28 @@ int main(void)
 {
     int n; 
     scanf("%d",&n);
-    // TODO
+    int *primos = (int*)malloc(n * sizeof(int));
+    if (!primos) return 1;
+
+    int count = 0;
+    for (int i = 2; count < n; i++) {
+        int es_primo = 1;
+        for (int j = 2; j <= sqrt(i); j++) {
+            if (i % j == 0) {
+                es_primo = 0;
+                break;
+            }
+        }
+        if (es_primo) {
+            primos[count++] = i;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (i) putchar(' ');
+        printf("%d", primos[i]);
+    }
+    putchar('\n');
+    free(primos);
     return 0;
 }
